9b1398c4ab285281fd6220a353aca4924b3378c8
Thierry Charbonnel <thierryc@users.noreply.github.com>
2017-12-06 07:57:08 -0500

    set cacheDirectory to false for babel-loader

--- next.config.js
+++ next.config.js
@@ -13,13 +13,19 @@ module.exports = {
   },
   //assetPrefix: '',
   assetPrefix: !debug ? '/Next-gh-page-example/' : '',
-  /* webpack: (config, { dev }) => {
+  webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
     // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
     // Important: return the modified config
     return config
-  },
+  }/*,
   webpackDevMiddleware: (config) => {
     // Perform customizations to webpack dev middleware config
     // console.log('webpackDevMiddleware');
