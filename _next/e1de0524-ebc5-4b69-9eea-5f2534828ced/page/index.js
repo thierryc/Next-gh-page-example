d246799cec4f1f2fb0bb9fb3ee1eaf1d19d2a7e3
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-02-14 10:11:09 -0500

    Deploy Next.js to gh-pages

--- _next/e1de0524-ebc5-4b69-9eea-5f2534828ced/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([4],{200:function(e,t,u){e.exports=u(201)},201:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(6),n=l(a),o=u(99),r=l(o);t.default=function(){return n.default.createElement("div",null,"Hello World. ",n.default.createElement(r.default,{href:"/about",as:"/Next-gh-page-example/about"},n.default.createElement("a",null,"About")))}}},[200]);
-            return { page: comp.default }
-          })
-        
